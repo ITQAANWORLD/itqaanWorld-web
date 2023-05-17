4fa09dfdd45110a40b4863e421e25091bf859288
phillipnzau <nzauonline@gmail.com>
2023-05-17 09:04:56 +0300

    navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        headerBlue:'hsl(214, 96%, 41%)',
         primaryBlue: ' 	hsl(222, 72%, 56%)',
         primaryBlack: ' 	hsl(237, 24%, 16%)',
         footerText:'hsl(215, 22%, 80%)',
         strokeColor:'#a0adc1',
-        darkText: 'hsl(0, 0%, 50%)'
+        darkText: 'hsl(0, 0%, 50%)',
+        headerBorder: 'rgba(0,0,0,.05)',
       }
     },
   },
