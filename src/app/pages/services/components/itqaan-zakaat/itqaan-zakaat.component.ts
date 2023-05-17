56e4525c643d8ff4cf8482cf26a28a8f9ff413fe
phillipnzau <nzauonline@gmail.com>
2023-05-17 20:16:55 +0300

    itqan zakaat page complete--pending bg images

--- src/app/pages/services/components/itqaan-zakaat/itqaan-zakaat.component.ts
+++ src/app/pages/services/components/itqaan-zakaat/itqaan-zakaat.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
+import { InfoContainerComponent } from 'src/app/shared/components/info-container/info-container.component';
+import { ItqaanServiceCardComponent } from 'src/app/shared/components/itqaan-service-card/itqaan-service-card.component';
 
 @Component({
   selector: 'app-itqaan-zakaat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InfoContainerComponent, ItqaanServiceCardComponent, FooterComponent],
   templateUrl: './itqaan-zakaat.component.html',
   styleUrls: ['./itqaan-zakaat.component.scss']
 })
 export class ItqaanZakaatComponent {
-
+  aboutItems = [
+    {
+        title: 'WHAT IS ZAKAAT?',
+        description: 'Zakat is a form of obligatory charity in Islam that involves giving a portion of one`s wealth to the less fortunate. It is one of the five pillars of Islam and is an act of worship that purifies both one`s wealth and soul. '
+    },
+  ];
+  itqaanServices = [
+    {
+      img: '../../../assets/imgs/sadaqah.svg',
+      title: 'Individual Zakaat'
+    },
+    {
+      img: '../../../assets/imgs/zakaat.svg',
+      title: 'Institutional Zakaat'
+    },
+   
+  ];
 }
