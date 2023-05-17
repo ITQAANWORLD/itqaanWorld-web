77096363a1d31562b16915be593e613929da5336
phillipnzau <nzauonline@gmail.com>
2023-05-17 20:50:05 +0300

    responsive start

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class NavbarComponent {
   @Input() scrolled: boolean = false;
+  isMenuOpen:boolean = false;
   
   services = [
     {title:'cash waqf',
