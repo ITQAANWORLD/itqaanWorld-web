759d58a6d72a9582e6076079c1b55d73141d06a5
phillipnzau <nzauonline@gmail.com>
2023-05-17 13:02:19 +0300

    navbar styling cont

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  services = ['cash waqf', 'itqaan sadaqa', 'itqaan zakaat', 'itqaan P2P loan', 'itqaan pay', 'takful','Al-Nisaa', 'atfaal', 'itqaan pension', 'itqaan BNB & rentals', 'hajj & umra savings ', 'itqaan transportation'];
+  quranHadith = ['quran', 'hadith']
 
 }
