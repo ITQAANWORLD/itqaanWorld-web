11592c9813cd5c6e24e332f3c1a3ed0de5ebaae0
phillipnzau <nzauonline@gmail.com>
2023-05-19 09:04:00 +0300

    contact page complete --pending action attactchemnt

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
-  selector: 'app-contact',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+    selector: 'app-contact',
+    standalone: true,
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss'],
+    imports: [CommonModule, FooterComponent]
 })
 export class ContactComponent {
 
