1c919b8e0369ab10190e571d1e175a309305e90b
phillipnzau <nzauonline@gmail.com>
2023-05-19 08:43:56 +0300

    shop page complete

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
-  selector: 'app-shop',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+    selector: 'app-shop',
+    standalone: true,
+    templateUrl: './shop.component.html',
+    styleUrls: ['./shop.component.scss'],
+    imports: [CommonModule, FooterComponent]
 })
 export class ShopComponent {
 
