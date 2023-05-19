3ecd0e49261e82569646caab776e295d37242380
phillipnzau <nzauonline@gmail.com>
2023-05-19 08:35:31 +0300

    terms page complete

--- src/app/pages/terms/terms.component.ts
+++ src/app/pages/terms/terms.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
-  selector: 'app-terms',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './terms.component.html',
-  styleUrls: ['./terms.component.scss']
+    selector: 'app-terms',
+    standalone: true,
+    templateUrl: './terms.component.html',
+    styleUrls: ['./terms.component.scss'],
+    imports: [CommonModule, FooterComponent]
 })
 export class TermsComponent {
 
