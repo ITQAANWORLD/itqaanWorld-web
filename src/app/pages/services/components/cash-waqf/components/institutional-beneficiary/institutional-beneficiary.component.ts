d342da07834c67d71d970556206111b2254cf85b
phillipnzau <nzauonline@gmail.com>
2023-05-25 15:16:49 +0300

    user journey

--- src/app/pages/services/components/cash-waqf/components/institutional-beneficiary/institutional-beneficiary.component.ts
+++ src/app/pages/services/components/cash-waqf/components/institutional-beneficiary/institutional-beneficiary.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../../../../../../shared/components/footer/footer.component";
 import { IconCardComponent } from "../../../../../../shared/components/icon-card/icon-card.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-institutional-beneficiary',
     standalone: true,
     templateUrl: './institutional-beneficiary.component.html',
     styleUrls: ['./institutional-beneficiary.component.scss'],
-    imports: [CommonModule, FooterComponent, IconCardComponent]
+    imports: [CommonModule, FooterComponent, IconCardComponent, RouterLink]
 })
 export class InstitutionalBeneficiaryComponent {
+
+  isInstitutionList:boolean = true;
+  isInstitutionSelected:boolean=false;
+  isDonate:boolean=false;
+  isPay:boolean=false;
+
   iconCards = [
     {
       title: 'Location',
@@ -33,5 +40,44 @@ export class InstitutionalBeneficiaryComponent {
       description:''
     }
   ]
+
+  toggleSelected(state:string) {
+    if (state == 'list') {
+      this.isInstitutionList = false
+      this.isPay = false;
+      this.isDonate = false;
+      this.isInstitutionSelected = true;
+
+    } else if(state == 'institution') {
+      this.isInstitutionList = false
+      this.isPay = false;
+      this.isInstitutionSelected = false;
+      this.isDonate = true;
+
+    } else if (state=='selected') {
+      this.isDonate = false;
+      this.isInstitutionList = false
+      this.isInstitutionSelected = false;
+      this.isPay = true;
+
+    }  else if (state =='backHome' ) {
+      this.isInstitutionList = true
+      this.isPay = false;
+      this.isDonate = false;
+      this.isInstitutionSelected = false;
+    }  else if (state == 'backList' ) {
+      this.isInstitutionList = false
+      this.isPay = false;
+      this.isDonate = false;
+      this.isInstitutionSelected = true;
+    }
+    else if (state == 'backSelected' ) {
+      this.isInstitutionList = false
+      this.isPay = false;
+      this.isInstitutionSelected = false;
+      this.isDonate = true;
+
+    }
+  }
 
 }
