d8515f19a8288c86869adfd7fb5b9fc79c11ed57
phillipnzau <nzauonline@gmail.com>
2023-06-01 14:20:00 +0300

    payment forms implemented, pending correct form type and visa icons

--- src/app/shared/components/selected-institution/selected-institution.component.ts
+++ src/app/shared/components/selected-institution/selected-institution.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -9,8 +9,37 @@ import { RouterLink } from '@angular/router';
   templateUrl: './selected-institution.component.html',
   styleUrls: ['./selected-institution.component.scss']
 })
-export class SelectedInstitutionComponent {
+export class SelectedInstitutionComponent implements OnInit {
   @Input() widthValueFromBackend: number = 60;
+  details:any
+  
+  // @ts-ignore
+  selectedDetailsId: string = null;
+
+  selectDetails(detailsId: string, event:Event) {
+    event.preventDefault();
+    if (this.selectedDetailsId === detailsId) {
+      // @ts-ignore
+      this.selectedDetailsId = null; // Close the currently opened details
+    } else {
+      this.selectedDetailsId = detailsId; // Open the clicked details
+    }
+  }
+
+  ngOnInit() {
+    this.details = document.querySelectorAll("details");
+    // Add the onclick listeners.
+    this.details.forEach((targetDetail:any) => {            
+        targetDetail.addEventListener("click", () => {
+        // Close all the details that are not targetDetail.
+        this.details.forEach((detail:any) => {
+            if (detail !== targetDetail) {
+            detail.removeAttribute("open");
+            }
+        });
+        });
+    });
+}
 
 
 }
